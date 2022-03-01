84778b167203ac10e13d798adbf2874e08c48e6a
jmsb8888 <80297741+jmsb8888@users.noreply.github.com>
2022-03-01 10:55:57 -0500

    prueba de poder editar archivo

--- src/run/RunApp.java
+++ src/run/RunApp.java
@@ -5,6 +5,6 @@ import presenters.Presenter;
 public class RunApp {
     public static void main(String[] args) {
         Presenter presenter = new Presenter();
-        System.out.println("hola - prueba");
+        System.out.println("hola - prueba-de poder editar");
     }
 }
