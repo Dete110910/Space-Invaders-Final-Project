eef9e5eb91955d66a457d7f66cf6be1ad8a9ca41
Fabian <fadasahe2002@gmail.com>
2022-03-08 14:01:00 -0500

    first commit fabian

--- src/run/RunApp.java
+++ src/run/RunApp.java
@@ -6,5 +6,6 @@ public class RunApp {
     public static void main(String[] args) {
         Presenter presenter = new Presenter();
         System.out.println(" prueba-de poder editar");
+        System.out.println("fabian");
     }
 }
